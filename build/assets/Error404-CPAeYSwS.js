aff5b8cc26a34b131f365413a6bc015663e55d96
Alex  Alex <user@MacBook-Pro-user.local>
2025-03-24 17:27:31 +0300

    build

---
+++ build/assets/Error404-CPAeYSwS.js
@@ -0,0 +1,1 @@
+import{ao as r}from"./index-Cd89jSuE.js";import{P as o,M as s}from"./index-C7MowBdE.js";const a=()=>r.jsx(o,{template:"entry",children:r.jsx(s,{id:"components.routes.pages.Error404.body"})});export{a as default};
