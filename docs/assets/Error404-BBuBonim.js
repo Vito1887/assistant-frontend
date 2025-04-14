7143174f3905b03b7c8d343aa699fe543319ffbf
Alex  Alex <user@MacBook-Pro-user.local>
2025-04-14 15:10:49 +0300

    reassembly

---
+++ docs/assets/Error404-BBuBonim.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B7Er15gc.js";import{P as o,M as s}from"./index-B8Rsk4q8.js";const a=()=>r.jsx(o,{template:"entry",children:r.jsx(s,{id:"components.routes.pages.Error404.body"})});export{a as default};
